d356caf9031ea58a519c9e5e949bcf33b988c851
Gádor <gador8@gmail.com>
2025-10-31 01:07:07 +0100

    Cambiada animación home e implementación de intersectionObserver

--- src/components/home/render-home.js
+++ src/components/home/render-home.js
@@ -1,30 +1,36 @@
 import homeHTML from "./home.html?raw";
 
-const imageListener = (imgContainer) => {
-    imgContainer.classList.toggle("active");
-};
-
-
 /**
  * Renderiza la sección de inicio.
  * @returns {HTMLElement} Ejemplo: <section></section>
  */
 export const renderHome = () => {
-    const homeElement = document.createElement("section");
-    homeElement.setAttribute("id", "home");
-    homeElement.innerHTML = homeHTML;
+	const homeElement = document.createElement("section");
+	homeElement.setAttribute("id", "home");
+	homeElement.innerHTML = homeHTML;
 
-    const imgContainer = homeElement.querySelector(".welcome-container");
-    imgContainer.addEventListener("click", imageListener(imgContainer));
-    return homeElement;
+    activateHeroAnimation(homeElement);
+
+	return homeElement;
 };
 
-// TODO: Animación con Intersection Observer, habría que especificar la clase .animate en el CSS
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("animate");
-//     }
-//   });
-// });
-// observer.observe(document.querySelector("#home"));
+const activateHeroAnimation = (parentElement) => {
+	const hero = parentElement.querySelector("header");
+
+	const observer = new IntersectionObserver(
+		(entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					parentElement.classList.add("active");
+					hero.classList.add("active");
+				} else {
+					parentElement.classList.remove("active");
+					hero.classList.remove("active");
+				}
+			});
+		},
+		{ threshold: 0.5 }
+	);
+
+	observer.observe(parentElement);
+};
